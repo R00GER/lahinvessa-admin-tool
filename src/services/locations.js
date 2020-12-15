3d710790f976c18df6e852b9da931e940412bd5f
R00GER <roni.friman@sonit.fi>
2020-12-15 23:02:59 +0200

    fix environment variables

--- src/services/locations.js
+++ src/services/locations.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.REACT_APP_API_ENDPOINT;
+const baseUrl = process.env.REACT_APP_API_ENDPOINT;
 // const baseUrl = 'http://localhost:3001/api/locations';
 
 const getLocations = async () => {
